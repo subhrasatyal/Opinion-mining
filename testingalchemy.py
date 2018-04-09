483b57d2adf2069489b52f0f022a97f9fb184c4b
Subhra Satyal <subhrasatyal@gmail.com>
2018-04-09 16:07:12 +0545

    Update testingalchemy.py

--- testingalchemy.py
+++ testingalchemy.py
@@ -1,11 +1,11 @@
-from sqlalchemy import *
-from sqlalchemy import create_engine
+
+from sqlalchemy import create_engine, MetaData, Table
 engine = create_engine('mysql://subhra:anything@localhost:5432/yelp_db')
 engine.echo = False
 connection = engine.connect()
 metadata = MetaData(engine)
-review = Table('review', metadata, autoload=True)
-s = review.select()
+review = Table('review', metadata, autoload_with=engine)
+s = review.select().order_by(func.rand()).limit(20)
 rs = s.execute()
 row = rs.fetchone()
 print 'id:', row[0]
@@ -13,6 +13,12 @@ print 'text:', row['text']
 print 'date:', row.date
 for row in rs:
     print row.id, 'has written the review', row.text, 'on date', row.date
+def main():
+  print("2o reviews by date")
+  
+if __name__== "__main__":
+  main()
 
+print("Reviews")
 
 
