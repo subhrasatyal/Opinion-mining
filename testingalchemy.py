3cf28ff76fd781beeebad562ae4d5220672cd1a9
Subhra Satyal <subhrasatyal@gmail.com>
2018-04-15 14:00:36 +0545

    Update testingalchemy.py

--- testingalchemy.py
+++ testingalchemy.py
@@ -9,21 +9,22 @@ review = Table('review', metadata, autoload_with=engine)
 
 class review(object):
     pass
-def loadSession():
-mapper(reviews, review)
 
-Session = sessionmaker(bind=engine)
-session = Session()
-return session
+def loadSession():
+    mapper(reviews, review)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    return session
 
 def main():
-  print("20 reviews by date")
-s = review.select().order_by(func.rand()).limit(20)
-rs = s.execute()
-row = rs.fetchall()
-print 'id:', row[0]
-print 'text:', row['text']
-print 'date:', row.date
+    print("20 reviews by date")
+    s = review.select().order_by(func.rand()).limit(20)
+    rs = s.execute()
+    row = rs.fetchall()
+    print 'id:', row[0]
+    print 'text:', row['text']
+    print 'date:', row.date
 for row in rs:
     print row.id, 'has written the review', row.text, 'on date', row.date
   
