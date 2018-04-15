5eccd68f90b04d04117740d2930ba67767d5fef8
Subhra Satyal <subhrasatyal@gmail.com>
2018-04-15 14:34:12 +0545

    nltk

---
+++ nltktest.py
@@ -0,0 +1,25 @@
+import nltk
+import math
+
+import re
+positiverange4 = ['legendary', 'legend', 'finest', 'insane', 'best']
+positiverange3 = ['favorite',  'excellent' ,'favourite', 'fav', 'delicious', 'awesome', 'perfect', 'perfection', 'perfectly', 'nice']
+positiverange2 = ['love', 'courteous', 'great', 'generous', 'tasty', 'pleasent', 'polite']
+positiverange1 = ['like', 'enjoyable', 'enjoy', 'reasonable', 'huge', 'plentiful', 'plenty', 'quick', 'enjoyed', 'fast', 'swift']
+neutralrange   = ['ok', 'fine', 'good', 'nice', 'gud', 'friendly', 'fresh', 'cheap']
+negativerange1 = ['crowded', 'lousy', 'slow', 'bad']
+negativerange2 = ['rude', 'worse', 'undercooked']
+negativerange3 = ['filthy']
+negativerange4 = ['worst', 'terrible', 'horrible', 'disgusting']
+val=0
+count = 0
+
+           
+
+    
+
+
+
+           
+
+
