3938e26e2c37ceb41473035b976d63be991361b0
Subhra Satyal <subhrasatyal@gmail.com>
2018-04-17 13:49:12 +0545

    Update testingalchemy.py

--- testingalchemy.py
+++ testingalchemy.py
@@ -1,53 +1,58 @@
-from sqlalchemy import create_engine, MetaData, Table, Column, Integer
-from sqlalchemy.orm import mapper
+from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String
 from sqlalchemy.sql.expression import func, select
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-#from sqlalchemy.ext.automap import automap_base
 
-engine = create_engine('mysql://subhra:anything@localhost:5432/yelp_db')
+
+engine = create_engine('mysql://subhra:anything@localhost:5432/yelp_db?charset=utf8')
 engine.echo = True
 Base = declarative_base()
-#Base = automap_base()
 connection = engine.connect()
 metadata = MetaData(engine)
 review = Table('review', metadata, autoload_with=engine)
 
-metadata.reflect(engine, only=['review']
+metadata.reflect(engine, only=['review'])
 
-#Table('review', metadata,
- #              Column('id', Integer, primary_key=True),
-  #              Column('user_id', ForeignKey('user.id'))
-   #         )
-
-
-Base = automap_base(metadata=metadata)
-Base.prepare()
 
 class Review(Base):
     __tablename__ = 'review'
-
     id = Column(Integer, primary_key=True)
+    business_id = Column(Integer, nullable=False)
+    user_id = Column(String(20), nullable=False)
+    stars = Column(Integer, nullable=True)
+    date = Column(String(20), nullable=True)
+    text = Column(String, nullable=True)
+    useful = Column(Integer, nullable=True)
+    funny = Column(Integer, nullable=True)
+    cool = Column(Integer, nullable=True)
+    
+    def __init__(self, text='Warm ambience'):
+        self.text = text
 
 def loadSession():
-    mapper(reviews, review)
-    
-    session = Session(engine)
-    session.commit()
+    # create session
+    Session = sessionmaker()
+    Session.configure(bind=engine)
+    session = Session()
 
-def main():
-    print("20 reviews by date")
-    s = review.select().order_by(func.rand()).limit(20)
-    row = rs.fetchall()
-    print 'id:', row[0]
-    print 'text:', row['text']
-    print 'date:', row.date
-    for row in rs:
-        print row.id, 'has written the review', row.text, 'on date', row.date
+
+def Main():
+    def __init__(self):
+        pass
+    def run(self):
+        s = review.select().order_by(func.rand()).limit(20)
+        row = rs.fetchall()
+        rs = s.execute()
+        print 'id:', row[0]
+        print 'text:', row['text']
+        print 'date:', row.date
+        for row in rs:
+           print row.id, 'has written the review', row.text, 'on date', row.date
   
-if __name__== "__main__":
+if __name__== '__main__':
     session = loadSession()
-    rs = s.execute()
+    
    # rs = session.query(reviews).all()
-    main()
+    Main()
+    connection.close()
 
